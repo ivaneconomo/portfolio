bbf6bde491e8c0bb74d094bcf845f4b694281d6c
ivaneconomo <104017304+ivaneconomo@users.noreply.github.com>
2023-06-05 16:32:39 -0300

    update wip

--- src/components/Main/Projects/Projects.jsx
+++ src/components/Main/Projects/Projects.jsx
@@ -7,22 +7,22 @@ const Projects = () => {
         Proyectos en los que participé
       </h2>
 
-      <article className='space-y-4 rounded bg-slate-300 pb-4 dark:bg-slate-800 border-slate-500/50 border'>
-        <div className='h-40 w-full rounded-t bg-[url("src/assets/img/hyper_estrenos_project.png")] bg-cover'>
-          <div className='flex h-40 w-full items-end rounded-t bg-gradient-to-t from-slate-300 px-4'>
-            <h3 className='bg-gradient-to-r from-yellow-500 to-red-500 bg-clip-text text-xl font-extrabold text-transparent backdrop-blur rounded'>
+      <article className='space-y-4 rounded border border-slate-50/30 bg-slate-50 pb-4 dark:border-slate-950/30 dark:bg-slate-950'>
+        <div className='flex h-60 w-full items-end rounded-t bg-[url("../../../src/assets/img/hyper_estrenos_project.png")] bg-cover bg-top'>
+          <div className='flex h-36 w-full items-end rounded-t bg-gradient-to-t from-slate-50 via-slate-50/30 dark:from-slate-950 dark:via-slate-950/60'>
+            <h3 className='container bg-gradient-to-r from-yellow-500 to-red-500 bg-clip-text text-xl font-extrabold text-transparent'>
               Hyper Estrenos
             </h3>
           </div>
         </div>
 
-        <p className='px-4'>
+        <p className='container'>
           Mi primer desafío como desarrollador tuvo como objetivo aplicar
           nuestro conocimiento en HTML, CSS y Bootstrap5. Teniendo en cuenta las
           buenas prácticas de la programación, y el diseño responsive.
         </p>
 
-        <div className='flex items-center space-x-4 px-4'>
+        <div className='container flex items-center space-x-4'>
           <a
             className='group flex space-x-2 text-gray-950/50 underline transition hover:text-gray-950 dark:text-gray-50/50 dark:hover:text-gray-50'
             href='https://github.com/ivaneconomo/hyper-estrenos'
