7f5f113c6d6e82484a100537ed3c3002d691ccf5
ivaneconomo <104017304+ivaneconomo@users.noreply.github.com>
2023-06-07 14:12:11 -0300

    update

--- src/components/Main/Banner/Banner.jsx
+++ src/components/Main/Banner/Banner.jsx
@@ -22,7 +22,7 @@ const Banner = () => {
       </div>
 
       <div className='flex items-center space-x-5'>
-        <button className='flex items-center space-x-1 rounded bg-pistachio-500 px-2 py-0.5 transition hover:bg-pistachio-600 active:ring-1 dark:hover:bg-pistachio-300'>
+        <button className='flex items-center space-x-1 rounded bg-pistachio-500 px-2 py-0.5 transition hover:bg-pistachio-600 active:ring-1 dark:hover:bg-pistachio-200'>
           <DownloadCVIcon />
           <span className='text-gray-950'>Descargar CV</span>
         </button>
