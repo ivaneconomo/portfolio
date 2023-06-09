ba726467b337d7eb2e6f5c311ef766aa7be26552
ivaneconomo <104017304+ivaneconomo@users.noreply.github.com>
2023-06-09 16:30:47 -0300

    update

--- src/components/molecules/AppViewport/AppViewport.jsx
+++ src/components/molecules/AppViewport/AppViewport.jsx
@@ -18,13 +18,12 @@ const AppViewport = () => {
   }, []);
 
   return (
-    <div className='fixed bottom-3 left-4 right-4 mx-auto flex flex-col justify-center space-x-3 space-y-2 rounded bg-fuchsia-500/50 p-2 text-center backdrop-blur-sm'>
+    <div className='text-xs fixed bottom-3 mx-auto flex flex-col justify-center space-x-3 space-y-1 rounded bg-pistachio-500/20 p-2 text-center backdrop-blur-sm'>
       <span>Viewport Size</span>
       <div className='flex justify-center'>
         <div className='mr-2 border-r pr-2'>Width: {viewportWidth}px</div>
         <div>Height: {viewportHeight}px</div>
       </div>
-      <span className='font-bold'>Sitio web en preparación.</span>
     </div>
   );
 };
--- src/components/organisms/Footer/Footer.jsx
+++ src/components/organisms/Footer/Footer.jsx
@@ -2,8 +2,9 @@ import AppViewport from '../../molecules/AppViewport/AppViewport';
 
 const Footer = () => {
   return (
-    <footer className='h-32 bg-slate-50 text-gray-950 dark:bg-slate-950 dark:text-gray-50'>
+    <footer className='h-28 bg-slate-50 text-gray-950 dark:bg-slate-950 dark:text-gray-50 text-center items-center flex justify-center'>
       <AppViewport />
+      <span className='font-bold mb-14'>Sitio web en preparación.</span>
     </footer>
   );
 };
