edc3ebb1d18f3e89c7a5f0bc6e4974091f78f5e5
ivaneconomo <104017304+ivaneconomo@users.noreply.github.com>
2023-06-10 03:12:43 -0300

    update

--- src/components/atoms/DownloadButton/DownloadButton.jsx
+++ src/components/atoms/DownloadButton/DownloadButton.jsx
@@ -1,11 +1,16 @@
 import { DownloadCVIcon } from '../icons/icons';
-import cv from '../../../../public/cv_es.pdf'
+import cvpdf from '../../../../public/cv_es.pdf';
 
 const DownloadButton = () => {
+  const handleDownload = () => {
+    window.location.href = cvpdf;
+  };
+
   return (
     <a
-      href={cv}
+      href={cvpdf}
       download
+      onClick={handleDownload}
       className='flex items-center space-x-1 rounded bg-pistachio-500 px-2 py-0.5 transition hover:bg-pistachio-600 active:ring-1 dark:hover:bg-pistachio-200'
     >
       <DownloadCVIcon />
