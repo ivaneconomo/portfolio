e02c642679d684b7eeedb16079340bde6fb30210
ivaneconomo <104017304+ivaneconomo@users.noreply.github.com>
2023-06-10 03:06:23 -0300

    update

--- src/components/atoms/DownloadButton/DownloadButton.jsx
+++ src/components/atoms/DownloadButton/DownloadButton.jsx
@@ -1,9 +1,10 @@
 import { DownloadCVIcon } from '../icons/icons';
+import cv from '../../../../public/cv_es.pdf'
 
 const DownloadButton = () => {
   return (
     <a
-      href='./cv_es.pdf'
+      href={cv}
       download
       className='flex items-center space-x-1 rounded bg-pistachio-500 px-2 py-0.5 transition hover:bg-pistachio-600 active:ring-1 dark:hover:bg-pistachio-200'
     >
